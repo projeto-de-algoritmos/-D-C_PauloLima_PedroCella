f5264563e62477c1ed321a4921df91277f5a4488
Pedro Cella <pedrovscella@gmail.com>
2021-04-06 12:16:07 -0300

    Adicionando templates de mergesort e count inversions

---
+++ main.py
@@ -0,0 +1,6 @@
+import countinversions
+import mergesort_countinversion
+
+print("Bem vindo ao contador de Ordenação de Array!")
+print("O objetivo desse algoritmo é comparar o nível de ordenação de um Array, que pode ou não ser dado pelo usuário, utilizando de diversos algoritmos de ordenação")
+print("A ordem dos algoritmos sempre será:\n1- Bubblesort\n3- Insertionsort\n3- Selectionsort\n4- Quicksort\n5- Mergesort")
